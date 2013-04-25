101b536d5b1bf54cfbbbf0b713e9c48c4884d425
patrickkeller <byteclic@gmail.com>
2013-04-25 09:26:58 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcidWebinterface::Application.config.secret_token = '5c0068588569a120c462cec4ee6fde23d6b1764e7724a555d9ee4afdef8484490428efce82242089b0135740e215d8cd4e12ab3ac65a16724f6505d4b5ef70ff'
